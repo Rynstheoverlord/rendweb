07bb8a830abc45e4900bc05a4970ce517d731eb2
Rynstheoverlord <107300355+Rynstheoverlord@users.noreply.github.com>
2024-05-29 02:57:10 +0100

    Update Rend.py

--- Rend.py
+++ Rend.py
@@ -12,11 +12,11 @@ if image is not None:
 
         image = image.filter(ImageFilter.GaussianBlur(blur))
         image = image.filter(ImageFilter.UnsharpMask(contrast))
-        
+
         with st.popover("Flip tools"):
             flip_x = st.checkbox("Flip X")
             flip_y = st.checkbox("Flip Y")
-    
+
             if flip_x:
                 image = ImageOps.mirror(image)
             if flip_y:
@@ -25,8 +25,9 @@ if image is not None:
         st.image(image, use_column_width=True)
 
         with st.popover("Download image"):
+            image_name = st.text_input("Enter image name", value="image")
             image_format = st.selectbox("Image format", ['png', 'jpg', 'webp', 'tiff', "gif", 'jfif'])
             bio = BytesIO()
             image.save(bio, 'PNG')
-            if st.download_button("Download image", bio, f"image.{image_format}", type="primary", use_container_width=True):
+            if st.download_button("Download image", bio, f"{image_name}.{image_format}", type="primary", use_container_width=True):
                 st.toast("Image saved successfully!")
